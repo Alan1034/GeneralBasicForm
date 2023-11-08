81e040063f15e1e48f7290a4140ddc1292fe1507
Chendeli <419287484@qq.com>
2023-11-08 10:34:11 +0800

    更新打包

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,15 @@
 /*
  * @Author: 陈德立*******419287484@qq.com
  * @Date: 2021-08-20 17:05:30
- * @LastEditTime: 2021-08-20 17:15:27
+ * @LastEditTime: 2023-11-07 15:05:39
  * @LastEditors: 陈德立*******419287484@qq.com
  * @Github: https://github.com/Alan1034
  * @Description: 
- * @FilePath: \GeneralBasicForm\src\index.js
+ * @FilePath: \GeneralBasicForm\src\index.ts
  * 
  */
+
 import GeneralBasicForm from './GeneralBasicForm.vue';
-export default GeneralBasicForm
+
+export const VGeneralBasicForm = GeneralBasicForm
+
